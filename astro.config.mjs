8241045e50a1f1c45f23666282886d98f61b883b
brianrodriguetz <bsrodriguezo@unbosque.edu.co>
2026-02-10 14:06:07 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://javierdvlpr.github.io',
+  site: 'https://brianrodriguetz.github.io',
   output: 'static',
 
   vite: {
